5e2bdba0ce515f125a4d208fd1b52729b4b9c5c3
Eng.Wesam <wesam@engwesams-mbp.mynet>
2023-02-07 17:56:26 +0200

    commit 101

--- config/app.php
+++ config/app.php
@@ -181,6 +181,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        niklasravnsborg\LaravelPdf\PdfServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -211,6 +212,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
+    'PDF' => niklasravnsborg\LaravelPdf\Facades\Pdf::class
         // 'ExampleClass' => App\Example\ExampleClass::class,
     ])->toArray(),
 
