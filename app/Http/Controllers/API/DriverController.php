eb54095f7c73cb4a3d44cb718ae9089f0ace5c69
mohammedir <mohammedirheem2017@gmail.com>
2023-02-15 11:13:55 +0200

    2/15 v2 2023 by mohammed

--- app/Http/Controllers/API/DriverController.php
+++ app/Http/Controllers/API/DriverController.php
@@ -282,7 +282,7 @@ class DriverController extends Controller
             $driver = User::query()->find($request->user()->id);
             $car = Car::query()->where('user_id','=',$request->user()->id)->where('status','=',1)->get()->first();
             $available_transportion = TransportationRequests::query()->where('status','=',1)->where('vehicle_type','=',$driver->vehicle_type)
-                ->orWhere('status','!=',1)->where('driver_id','=',$request->user()->id)->get();
+                ->orWhere('status','!=',1)->where('driver_id','=',$request->user()->id)->where('vehicle_type','=',$driver->vehicle_type)->get();
             if ($car){
                 foreach ($available_transportion as $mytransportation){
                     $place = Map::query()->where('lat','=',$mytransportation->lat_to)->where('long','=',$mytransportation->lng_to)->get()->first();
