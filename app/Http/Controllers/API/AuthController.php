4746eacb149fcd9a845ad02a7c2aef6c4618a8db
mohammedir <mohammedirheem2017@gmail.com>
2023-02-26 11:09:39 +0200

    2/26 v1 add loginUserStatus  2023 by mohammed

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -251,7 +251,7 @@ class AuthController extends Controller
                 ];
                 return $this->api_response(200, true, trans('api.login done'), $res, 200);
             }elseif ($data->user_status = 2){
-                return  $this->setError(200 ,false, trans('api.Your account has been suspended by admin') , 200);
+                return  $this->setError(400 ,false, trans('api.Your account has been suspended by admin') , 400);
             }
         }else{
             return  $this->setError(400 ,false, trans('api.user not found') , 400);
