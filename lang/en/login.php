5b4e3c94400b3f839ab4a99626eaf0b276aa67da
Eng.Wesam <wesam@EngWesams-MacBook-Pro.local>
2023-03-02 15:36:00 +0200

    commit 112

--- lang/en/login.php
+++ lang/en/login.php
@@ -5,7 +5,7 @@ return [
 
     'email' => 'Email',
     'password' => 'Password',
-    'Sign In to Tegoa' => 'Sign In to Tegoa',
+    'Sign In to Tegoa' => 'Sign In to Teqoa',
     'Please wait...' => 'Please wait...',
     'login' => 'login',
 
