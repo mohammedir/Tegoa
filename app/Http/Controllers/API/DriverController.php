7815f48986eac16029921450a2c4b143131e39cf
Mohammed <mohammedirheem2017@gmail.com>
2023-03-06 12:42:08 +0200

    3/6/2023 by mohammed

--- app/Http/Controllers/API/DriverController.php
+++ app/Http/Controllers/API/DriverController.php
@@ -286,7 +286,7 @@ class DriverController extends Controller
         try {
             $driver = User::query()->find($request->user()->id);
             $car = Car::query()->where('user_id','=',$request->user()->id)->get()->first();
-            $available_transportion = TransportationRequests::query()->where('status','!=',5)
+            $available_transportion = TransportationRequests::query()->where('status','=',1)
                 ->where('vehicle_type','=',$driver->vehicle_type)
                 ->orWhere('status','!=',1)->where('driver_id','=',$request->user()->id)->where('vehicle_type','=',$driver->vehicle_type)
                 ->orderBy('id', 'DESC')->get();
