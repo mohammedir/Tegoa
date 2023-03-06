b913602b4ae7e72d9c78829389b518d9394c9b97
Mohammed <mohammedirheem2017@gmail.com>
2023-03-06 12:05:05 +0200

    3/6/2023 by mohammed

--- app/Http/Controllers/API/DriverController.php
+++ app/Http/Controllers/API/DriverController.php
@@ -286,13 +286,13 @@ class DriverController extends Controller
             $driver = User::query()->find($request->user()->id);
             $car = Car::query()->where('user_id','=',$request->user()->id)->get()->first();
             $available_transportion = TransportationRequests::query()->where('status','!=',5)->where('vehicle_type','=',$driver->vehicle_type)
-                ->orWhere('status','!=',1)->where('driver_id','=',$request->user()->id)->where('vehicle_type','=',$driver->vehicle_type)
                 ->when($type == 'End_Trip', function ($query) use ($value) {
                     $query->where('status', '=', 4);
                 })
                 ->when($type == 'All', function ($query) use ($value) {
                     $query->where('status', '!=', 4);
                 })
+                ->orWhere('status','!=',1)->where('driver_id','=',$request->user()->id)->where('vehicle_type','=',$driver->vehicle_type)
                 ->orderBy('id', 'DESC')->get();
             if ($car->status == 1){
                 foreach ($available_transportion as $mytransportation){
