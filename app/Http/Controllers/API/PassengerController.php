a2a2828c4a9d31a03a849380c78f5d2c88cf1da3
Mohammed <mohammedirheem2017@gmail.com>
2023-03-07 10:02:51 +0200

    3/6/2023 by mohammed

--- app/Http/Controllers/API/PassengerController.php
+++ app/Http/Controllers/API/PassengerController.php
@@ -303,7 +303,7 @@ class PassengerController extends Controller
                 })
                 ->orderBy('id', 'DESC')->get();
             foreach ($Mytransportation as $mytransportation){
-                $departure_time = Carbon::parse($mytransportation->departure_time);
+                //$departure_time = Carbon::parse($mytransportation->departure_time);
                /* if ($mytransportation->status == 1 && $departure_time->lessThan($time)){
                     TransportationRequests::query()->where('id', $mytransportation->id)->update(['status' => 5]);
                 }*/
