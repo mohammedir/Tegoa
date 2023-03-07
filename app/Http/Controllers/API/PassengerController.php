1127f9b9740a088f5855b7ca371bd42eabb82b9a
Mohammed <mohammedirheem2017@gmail.com>
2023-03-07 09:45:40 +0200

    3/6/2023 by mohammed

--- app/Http/Controllers/API/PassengerController.php
+++ app/Http/Controllers/API/PassengerController.php
@@ -189,11 +189,11 @@ class PassengerController extends Controller
         $pages = Pages::query()->find(6);
         $pages->increment('count');
         if ($validator->passes()){
-            $time = now()->format('h:i A');
+            /*$time = now()->format('h:i A');
             $departure_time = Carbon::parse($request->departure_time);
             if ($departure_time->lessThan($time)){
                 return  $this->setError(200,false, trans("api.The time entered for departure is incorrect. Please add a value greater than the current time") , 200);
-            }
+            }*/
             if ($passenger_id){
                     try {
                         $transportation_requests = new TransportationRequests();
